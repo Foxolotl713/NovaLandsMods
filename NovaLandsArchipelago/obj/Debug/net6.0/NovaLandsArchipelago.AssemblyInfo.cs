c33879b1068367b11685b77c6fb7facf9bb28896
Foxolotl713 <theo@n-i-m-z.de>
2026-09-10 14:47:09 +0200

    fix certain items

--- NovaLandsArchipelago/obj/Debug/net6.0/NovaLandsArchipelago.AssemblyInfo.cs
+++ NovaLandsArchipelago/obj/Debug/net6.0/NovaLandsArchipelago.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovaLandsArchipelago")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce1d224e2d05d30daf751a996ba485d97149a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c1cf8ca814a30ccf828ebc83007d38ec4c67f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovaLandsArchipelago")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovaLandsArchipelago")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
